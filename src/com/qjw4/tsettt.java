4b632c168a20ccd6b80c5c3fa1bb31e7ba92e51c
JinweiQ-11 <2826879775@qq.com>
2020-12-22 22:40:45 +0800

    Spring2

--- src/com/qjw4/tsettt.java
+++ src/com/qjw4/tsettt.java
@@ -2,9 +2,14 @@ package com.qjw4;
 
 public class tsettt {
 	public static void main(String args[]) {
+
+
 		String Stra="mldn";
 		String Strb="mldn";
 		String strb=new String("mldn");
 		System.out.println(Stra==Strb);
+
 	}
 }
+
+
